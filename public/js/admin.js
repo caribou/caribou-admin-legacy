108fc98829df9791cbd67a6641c1a959c1fa725a
ryan spangler <ryan.spangler@gmail.com>
2011-08-15 03:23:16 -0700

    nav and save button

--- public/js/admin.js
+++ public/js/admin.js
@@ -4,8 +4,8 @@ triface.admin = function() {
             return $('#contentTable').tmpl({modelname: modelname, headings: headings, items: items});
         };
 
-        var nav = function(choices) {
-            return $('#contentMenu').tmpl({choices: choices, classes: ''});
+        var nav = function(chosen, choices) {
+            return $('#contentMenu').tmpl({chosen: chosen || '', choices: choices, classes: ''});
         };
 
         var detail = function(modelname, headings, item) {
@@ -19,12 +19,24 @@ triface.admin = function() {
         };
     }();
 
-    var headerNav = function() {
+    var nav = function() {
+        var select = function(choice, url) {
+            $('#nav li').removeClass('selected');
+            $('#nav li.'+choice).addClass('selected');
+            triface.go(url);
+        };
+
+        return {
+            select: select
+        };
+    }();
+
+    var headerNav = function(modelname) {
         if ($('#header').html() == '') {
             var choices = _.map(triface.models, function(model) {
                 return {url: _.template('/<%= name %>', model), title: model.name};
             });
-            var nav = template.nav(choices);
+            var nav = template.nav(modelname, choices);
             $('#header').html(nav);
         }
     };
@@ -37,32 +49,50 @@ triface.admin = function() {
         triface.api.get({
             url: _.template('/<%= model %>', params),
             success: function(response) {
-                headerNav();
-                var body = template.table(params.model, _.keys(_.first(response)), response);
-                $('#triface').html(body);
+                headerNav(params.model);
+                if (response.length > 0) {
+                    var body = template.table(params.model, _.keys(_.first(response)), response);
+                    $('#container').html(body);
+                } else {
+                    $('#container').html('No items to show');
+                }
             }
         });
+    };
+
+    var contentNew = function(params, query) {
+        // triface.api.get({
+        //     url: _.template('/<%= model %>/<%= id %>', params),
+        //     success: function(response) {
+        //         headerNav(params.model);
+        //         var body = template.detail(params.model, _.keys(response), response);
+        //         $('#container').html(body);
+        //     }
+        // });
     };
 
     var contentDetail = function(params, query) {
         triface.api.get({
             url: _.template('/<%= model %>/<%= id %>', params),
             success: function(response) {
-                headerNav();
+                headerNav(params.model);
                 var body = template.detail(params.model, _.keys(response), response);
-                $('#triface').html(body);
+                $('#container').html(body);
             }
         });
     };
 
     triface.routing.add('/', 'home', home);
     triface.routing.add('/:model', 'contentList', contentList);
+    triface.routing.add('/:model/new', 'contentNew', contentNew);
     triface.routing.add('/:model/:id', 'contentDetail', contentDetail);
 
     return {
         init: function() {
             triface.init();
-        }
+        },
+
+        nav: nav
     };
 }();
 
