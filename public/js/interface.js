9b00b2e722b5c7fda96aeb95eb58dd897698ae61
ryan spangler <ryan.spangler@gmail.com>
2011-10-17 16:58:08 -0700

    addresses and decimal types

--- public/js/interface.js
+++ public/js/interface.js
@@ -161,7 +161,7 @@ var interface = function() {
     };
 
     var modelFieldTypes = function() {
-        return ['id', 'integer', 'string', 'slug', 'text', 'boolean', 'timestamp', 'asset', 'address', 'collection', 'part'];
+        return ['id', 'integer', 'decimal', 'string', 'slug', 'text', 'boolean', 'timestamp', 'asset', 'address', 'collection', 'part'];
     };
 
     var act = function() {
