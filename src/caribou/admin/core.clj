d434897d3faa8331416cb522cdaefc3e678d6850
ryan spangler <ryan.spangler@gmail.com>
2012-04-25 16:05:37 -0700

    now loading public from various places

--- src/caribou/admin/core.clj
+++ src/caribou/admin/core.clj
@@ -1,14 +1,34 @@
 (ns caribou.admin.core
   (:use compojure.core)
-  (:require [compojure.route :as route]
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]
+            [compojure.route :as route]
             [compojure.handler :as handler]))
+
+(def file-separator
+  (str (.get (java.lang.System/getProperties) "file.separator")))
+
+(defn pathify
+  [paths]
+  (string/join file-separator paths))
+
+(defn file-exists?
+  [path]
+  (.exists (io/file path)))
+
+(defn which-public
+  []
+  (let [admin-path (pathify ["admin" "public"])]
+    (if (file-exists? admin-path)
+      admin-path
+      "public")))
 
 (defn render-index
   [& args]
-  (slurp "public/caribou.html"))
+  (slurp (pathify [(which-public) "caribou.html"])))
 
 (defroutes admin-routes
-  (route/files "/" {:root "public"})
+  (route/files "/" {:root (which-public)})
   (route/not-found (render-index)))
 
 (declare app)
