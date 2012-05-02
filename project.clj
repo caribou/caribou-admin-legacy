cd09457fcff85dce293dc28a7eb2f0924ab69f15
eric feigner <eric@weareinstrument.com>
2012-05-02 14:30:40 -0700

    adding snapshot / release repos for jar publishin'

--- project.clj
+++ project.clj
@@ -11,5 +11,9 @@
   :ring {:handler caribou.admin.core/app
          :servlet-name "caribou-admin"
          :init caribou.admin.core/init
-         :port 33553})
+         :port 33553}
+  :repositories {"snapshots" {:url "http://battlecat:8080/nexus/content/repositories/snapshots" 
+                              :username "deployment" :password "deployment"}
+                 "releases"  {:url "http://battlecat:8080/nexus/content/repositories/releases" 
+                              :username "deployment" :password "deployment"}})
 
