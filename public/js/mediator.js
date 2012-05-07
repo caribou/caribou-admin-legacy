16d86a0588a70b3ecb01bec471f443c7a7f8aedb
Matthew Willhite <matt.willhite@weareinstrument.com>
2012-05-07 16:57:59 -0700

    Added queueing system to mediator to ensure we have the data we need

--- public/js/mediator.js
+++ public/js/mediator.js
@@ -3,9 +3,15 @@
   // Mediates all the action
   // This is the central hub for communication between modules
 
+  // Mediator has a queue property that allows us to stack up events
+  //   it waits until sync:modelData is fired before kicking them off
+  //   this might be a terrible idea
+
   // NOTE: Keep an eye on this file, it could get out of hand!
 
-  var mediator = app.mediator = {};
+  var mediator = app.mediator = {
+    queue: []
+  };
 
   _.extend(mediator, Caribou.Events);
 
@@ -15,15 +21,27 @@
     new app.views.GlobalNav({
       collection: modelData
     }).render();
+
+    app.synced = true;
+
+    while(mediator.queue.length)
+      mediator.queue.shift()();
   });
 
 
-  mediator.on('sync:genericModelData', function(modelData) {
-    var view = new app.views.GenericModelList({
-      collection: modelData
-    });
 
-    $('#active_admin_content').empty().append(view.render().el);
+  mediator.on('sync:genericModelData', function(modelData) {
+    var fn = function() {
+      var view = new app.views.GenericModelList({
+        collection: modelData
+      });
+
+      $('#active_admin_content').empty().append(view.render().el);
+    };
+
+    if(app.synced) return fn();
+
+    mediator.queue.push(fn);
   });
 
 
