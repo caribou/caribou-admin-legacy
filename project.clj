f8bda2a1979c0e42b21ca2a996125da357d0a79f
ryan spangler <ryan.spangler@gmail.com>
2012-08-09 15:47:04 -0700

    fixed boolean field defaults

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-admin "0.4.5"
+(defproject antler/caribou-admin "0.4.6"
   :description "Flexible and adaptive admin for caribou-api"
   :url "http://github.com/antler/caribou-admin"
   :license {:name "Eclipse Public License"
