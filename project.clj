b45cfdd0098fdbc67e45192e9cb61839d95d9f40
justin glenn smith <noisesmith@gmail.com>
2012-09-07 09:47:53 -0700

    "version bump"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-admin "0.4.7"
+(defproject antler/caribou-admin "0.4.8"
   :description "Flexible and adaptive admin for caribou-api"
   :url "http://github.com/antler/caribou-admin"
   :license {:name "Eclipse Public License"
