52500ca8466f97d1a38331f059100e9e4524b892
ryan spangler <ryan.spangler@gmail.com>
2013-02-08 15:57:21 -0800

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject antler/caribou-admin "0.8.0"
+(defproject antler/caribou-admin "0.8.1"
   :description "Flexible and adaptive admin for caribou-api"
   :url "http://github.com/antler/caribou-admin"
   :license {:name "Eclipse Public License"
